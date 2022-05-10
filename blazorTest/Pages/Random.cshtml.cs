3a19921e7885f2fe7922cbb35343dfb9ae8e9680
MasudurRah <mrahman709@outlook.com>
2022-05-10 18:34:09 -0400

    Worked on routing and learning to create and adding to list

---
+++ blazorTest/Pages/Random.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace blazorTest.Pages
+{
+    public class RandomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
